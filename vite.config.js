5939f08edb302575dd676c16ff23379dacb4c061
NovaX <56307137+mgawa@users.noreply.github.com>
2026-08-14 15:21:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  publicDir: "assets",
+
   build: {
     target: "es2022"
   },
+
   optimizeDeps: {
     exclude: ["@xmtp/browser-sdk"]
   }
